97c58b42e428ae4053507ce5fff169b24970a6c2
Francesco Salvestrini <salvestrini@gmail.com>
2012-08-19 19:21:22 +0200

    Rearranged logging related macros

---
+++ src/logs.cc
@@ -0,0 +1,8 @@
+//
+// (C) 2012 Francesco Salvestrini <salvestrini AT gmail DOT com>
+//
+
+#include "logs.hh"
+
+unsigned int __logs_level  = 0;
+const char * __logs_prefix = "<UNKNOWN>";
---
+++ src/logs.hh
@@ -0,0 +1,29 @@
+//
+// (C) 2012 Francesco Salvestrini <salvestrini AT gmail DOT com>
+//
+
+#ifndef LOGS_HH
+#define LOGS_HH
+
+#include <iostream>
+
+extern const char * __logs_prefix;
+extern unsigned int __logs_level;
+
+#define LOG_SET_PREFIX(PFX) do { __logs_prefix = PFX; } while (0)
+#define LOG_SET_LEVEL(LVL)  do { __logs_level  = LVL; } while (0)
+
+#define __LOG(MSG, COND)                                                \
+	do {                                                            \
+	        if (COND)                                               \
+	                std::cout << __logs_prefix << ": " << MSG       \
+				  << std::endl;                         \
+	} while (0)
+
+#define LDBG(MSG) do { __LOG(MSG, (__logs_level > 2)); } while (0)
+#define LVRB(MSG) do { __LOG(MSG, (__logs_level > 1)); } while (0)
+#define LMSG(MSG) do { __LOG(MSG, true);               } while (0)
+#define LWRN(MSG) do { __LOG(MSG, true);               } while (0)
+#define LERR(MSG) do { __LOG(MSG, true);               } while (0)
+
+#endif
